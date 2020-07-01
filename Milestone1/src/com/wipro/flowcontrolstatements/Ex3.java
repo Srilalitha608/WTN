aa72dffd7303fca313cd7357aec1d15b0089e224
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 23:02:42 +0530

    print all values in a line separated by comma

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex3.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex3 {
+
+	public static void main(String[] args) 
+	{
+		if(args.length==0)
+			System.out.println("No values");
+		else
+			System.out.println(args[0]+","+args[1]);
+	}
+
+}
