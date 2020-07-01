885b1a9c237c4b5b371f0b30976556f853e5b9e9
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 23:33:14 +0530

    print prime numbers in given range

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex13.java
@@ -0,0 +1,22 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex13 {
+
+	public static void main(String[] args) 
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		for(int i=a;i<=b;i++)
+		{
+			int count=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+					count++;
+			}
+			if(count==2)
+				System.out.print(i+" ");
+		}
+	}
+
+}
