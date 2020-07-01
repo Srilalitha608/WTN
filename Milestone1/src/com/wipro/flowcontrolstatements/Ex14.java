c28766711b16b5500747f1db72b6ae5617b53403
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 23:36:07 +0530

    print sum of all digits of a given number

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex14.java
@@ -0,0 +1,19 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex14 {
+
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		int r=0;
+		int sum=0;
+		while(n!=0)
+		{
+			r=n%10;
+			sum=sum+r;
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+
+}
