2a420c9c3a442faa546cd6200b8af887fedafb13
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 23:16:22 +0530

    convert to uppercase or lowercase

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex7.java
@@ -0,0 +1,22 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex7 {
+
+	public static void main(String[] args) 
+	{
+		String str=args[0];
+		char c=str.charAt(0);
+		char temp;
+		if(c>='A'&&c<='Z')
+		{
+			temp=Character.toLowerCase(c);
+			System.out.println(c+"->"+temp);
+		}
+		else if(c>='a'&&c<='z')
+		{
+			temp=Character.toUpperCase(c);
+			System.out.println(c+"->"+temp);
+		}
+	}
+
+}
