79275ae96c1e57ef99ad846cd70e0bb436fcdcdb
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 23:44:31 +0530

    palindrome or not

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex17.java
@@ -0,0 +1,22 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex17 {
+
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		int r=0,temp,sum=0;
+		temp=n;
+		while(n!=0)
+		{
+			r=n%10;
+			sum=sum*10+r;
+			n=n/10;
+		}
+		if(sum==temp)
+			System.out.println(temp+" is a palindrome");
+		else
+			System.out.println(temp+" is not a palindrome");
+	}
+
+}
