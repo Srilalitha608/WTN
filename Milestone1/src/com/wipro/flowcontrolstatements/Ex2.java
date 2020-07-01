2b8c038b1da14e8c20cae2376175cc6ca4085811
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-01 22:55:06 +0530

    even or odd

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,14 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+
+	public static void main(String[] args) 
+	{
+		int n=Integer.parseInt(args[0]);
+		if(n%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
