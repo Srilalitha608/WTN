d55300a81abdc14092e45a6da012867cd3398e17
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 15:45:09 +0530

    print true if every element is 1 or 4

---
+++ Milestone1/src/com/wipro/arrays/Ex11.java
@@ -0,0 +1,25 @@
+package com.wipro.arrays;
+import java.util.Scanner;
+public class Ex11 {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,c=0;
+		int[] a=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(a[i]!=1&&a[i]!=4)
+				c++;
+		}
+		if(c==0)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+}
