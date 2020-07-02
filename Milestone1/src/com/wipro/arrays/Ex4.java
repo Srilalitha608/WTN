ff333230d8aede0735d9b372c0e747b7ad0e9045
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-02 14:54:45 +0530

    print corresponding character for given ascii values

---
+++ Milestone1/src/com/wipro/arrays/Ex4.java
@@ -0,0 +1,22 @@
+package com.wipro.arrays;
+import java.util.Scanner;
+public class Ex4 {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		char c;
+		int i;
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			c=(char)a[i];
+			System.out.print(c+"\t");
+		}
+	}
+}
