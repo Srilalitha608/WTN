5766756e6677ad97db75c6f9d634ecfa3b0033b7
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-03 18:11:17 +0530

    inheritance2

---
+++ Milestone1/src/com/wipro/inheritance/TestEmployee.java
@@ -0,0 +1,10 @@
+package com.wipro.inheritance;
+public class TestEmployee {
+	public static void main(String[] args) 
+	{
+		Person p= new Person("Lalitha");
+		Employee e=new Employee("Pooja",10000,2017,"12345");
+		System.out.println(p);
+		System.out.println(e);
+	}
+}
