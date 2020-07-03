e5d46139448432fced44a84ae25d0a22f7c2937a
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-07-03 18:42:55 +0530

    polymorphism2

---
+++ Milestone1/src/com/wipro/polymorphism/Shape.java
@@ -0,0 +1,12 @@
+package com.wipro.polymorphism;
+public class Shape 
+{
+	void draw()
+	{
+		System.out.println("Drawing Shape");
+	}
+	void erase()
+	{
+		System.out.println("Erasing Shape");
+	}
+}
---
+++ Milestone1/src/com/wipro/polymorphism/Triangle.java
@@ -0,0 +1,12 @@
+package com.wipro.polymorphism;
+public class Triangle extends Shape
+{
+	void draw()
+	{
+		System.out.println("Drawing Triangle");
+	}
+	void erase()
+	{
+		System.out.println("Erasing Traingle");
+	}
+}
